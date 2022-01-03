948931d90e95dbf901b117570ca54452fa24dc39
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2022-01-03 12:37:51 +0600

    code

---
+++ dot.js
@@ -0,0 +1,8 @@
+const age = [12, 14, 13, 17, 19];
+const ages = [12, 15, 18, 30];
+const allAges = age.concat(ages);
+console.log(allAges);
+
+const taka = [234, 450, 434];
+const takapoisa = Math.max(...taka);
+console.log(takapoisa);
